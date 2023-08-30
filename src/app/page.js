93f16ab47f7bbdf98809887f164f146eeb5600b7
nohssam73 <nohssam73@gmail.com>
2023-08-30 14:42:09 +0900

    Makeup API _console_확인

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,23 @@
-
+"use client"
+import { useEffect } from "react"
+import axios from "axios"
 export default function Home() {
+  const API_URL = "/api/v1/products.json?brand=maybelline";
+  const getData = () => {
+    axios.get(
+      API_URL
+    ).then(data =>{
+       console.log(data);
+    })
+  }
+  // 렌더링 될때마다 실행
+  // useEffect(() => {  });
+
+  // 최초 한번만 실행할때 
+  useEffect(() => {
+    getData();
+  },[]);
+
   return (
     <>
       <h1 id="main">main 창</h1>
